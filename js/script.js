9fc0a7a364b02590266828a1f1417e49e5a2b809
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-25 17:52:35 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+
+function checkNumber() {
+  // input
+  const userNumber = parseInt(document.getElementById("integer").value)
+
+  //process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You got it!"
+  }
+
+  if (userNumber != randomNumber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You didn't get it!"
+  }
+}
