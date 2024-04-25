156f9c34360b3dc6829b1900deba6bea63be9f20
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-25 18:40:48 +0000

    js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
 
-const 
-
 /**
  * This function checks if the user has selected the random number from above
  */
@@ -16,7 +14,19 @@ function checkNumber() {
   if (userAge >= 17) {
     // output
     document.getElementById("result").innerHTML =
-      "You got it!"
+      "You can see an R rated movie alone!"
+  }
+
+  if (userAge >= 13) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You can see a PG-13 movie alone!"
+  }
+
+  if (userAge >= 5) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You can see a G or PG movie alone!"
   }
 
   if (userNumber != randomNumber) {
